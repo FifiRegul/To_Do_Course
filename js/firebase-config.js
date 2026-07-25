186900e11767a6d79f0c6e7948c4e331159f7be8
FifiRegul <mitsou33520@gmail.com>
2026-07-25 14:28:52 +0200

    Maj & Co FireBase

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,17 +5,17 @@
 // Voir DEPLOIEMENT.md section "1. Créer le projet Firebase"
 // ============================================================
 const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY",
-  authDomain: "VOTRE_PROJET.firebaseapp.com",
-  projectId: "VOTRE_PROJET",
-  storageBucket: "VOTRE_PROJET.appspot.com",
-  messagingSenderId: "VOTRE_SENDER_ID",
-  appId: "VOTRE_APP_ID"
+  apiKey: "AIzaSyDlbQ7W0Vvc4znhHNVVLTSBzvAwCtG0T9Q",
+  authDomain: "todocourse-96295.firebaseapp.com",
+  projectId: "todocourse-96295",
+  storageBucket: "todocourse-96295.appspot.com",
+  messagingSenderId: "271519876241",
+  appId: "1:271519876241:web:bc5fcdf9f3168cb58feb30"
 };
 
 // Clé VAPID publique pour les notifications push (Firebase Console >
 // Cloud Messaging > Web Push certificates). Voir DEPLOIEMENT.md.
-const FCM_VAPID_KEY = "VOTRE_CLE_VAPID_PUBLIQUE";
+const FCM_VAPID_KEY = "BJ9dc-AOgb0p3vksI_nbxDQmtQtNjyfuS1R0ueIdNvNf0V7FVukFHH3rl9zirvLpxKwmmDW6LoPijpmP85JcWig";
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
